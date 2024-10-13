db8dd5575498dc22e83300c1e37255c4f80219be
MofajjelBD <mofajjel.bd@gmail.com>
2024-10-14 02:14:43 +0600

    upadte fake data call and some alingment recover

--- src/Home/AllBooks/AllBooks.jsx
+++ src/Home/AllBooks/AllBooks.jsx
@@ -1,16 +1,26 @@
+import { useEffect, useState } from "react";
 import Book from "./Book.jsx";
 
 export const AllBooks = () => {
+  const [BookData, setBookData] = useState([]);
+  useEffect(() => {
+    fetch("BookData.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setBookData(data);
+      });
+  }, []);
   return (
     <div className="mt-24">
       <h2 className="font-bold text-4xl text-Primary font-Playfair text-center">
         Books
       </h2>
-      <div className="flex flex-wrap gap-6 mt-8">
-        <Book></Book>
-        <Book></Book>
-        <Book></Book>
-        <Book></Book>
+      <div className="flex flex-wrap justify-center gap-6 mt-8">
+        {BookData.map((BookData) => (
+          <Book key={BookData.bookId} BookData={BookData}></Book>
+        ))}
       </div>
     </div>
   );
--- src/Home/AllBooks/Book.jsx
+++ src/Home/AllBooks/Book.jsx
@@ -1,20 +1,29 @@
 import { Link } from "react-router-dom";
-import BannerImage from "./../../assets/banner.png";
 import { FaRegStar } from "react-icons/fa";
-const Book = () => {
+import PropTypes from "prop-types";
+const Book = ({ BookData }) => {
+  const { image, bookName, author, category, rating, tags } = BookData;
   return (
     <>
       <Link
         to="books/:1"
         className="card bg-base-100 w-[374px] shadow-xl border border-gray rounded-3xl dark:bg-slate-800"
       >
-        <figure className="px-6 py-6 w-full h-full">
+        <figure className="px-6 py-6 w-full h-full !items-start">
           <div className="bg-base-200 rounded-[16px] h-[230px] w-full p-8 justify-center flex w-full">
-            <img className="h-full w-auto" src={BannerImage}></img>
+            <img className="h-full w-auto" src={image}></img>
           </div>
         </figure>
         <div className="card-body font-Mulish pb-6 pt-0 px-6 justify-start">
           <div className="flex gap-4 flex-wrap">
+            {tags.map((tags, Index) => (
+              <span
+                key={Index}
+                className="text-base text-green-500 font-medium font-WorkSans rounded-full py-2 px-5 bg-green-50"
+              >
+                {tags}
+              </span>
+            ))}
             <span className="text-base text-green-500 font-medium font-WorkSans rounded-full py-2 px-5 bg-green-50">
               Young Adult
             </span>
@@ -23,18 +32,18 @@ const Book = () => {
             </span>
           </div>
           <h2 className="card-title text-2xl font-bold font-Playfair text-Primary">
-            The Catcher in the Rye
+            {bookName}
           </h2>
           <p className="text-base font-medium font-WorkSans text-Primary text-opacity-80">
-            By : Awlad Hossain
+            By : {author}
           </p>
           <div className="mt-4 border-dashed border-b-2"></div>
           <div className="flex justify-between flex-wrap py-3">
             <span className="text-base font-medium font-WorkSans text-Primary text-opacity-80 flex gap-2">
-              Fiction
+              {category}
             </span>
             <span className="text-base font-medium font-WorkSans text-Primary text-opacity-80 flex items-center gap-2">
-              5.00
+              {rating}
               <FaRegStar />
             </span>
           </div>
@@ -42,5 +51,8 @@ const Book = () => {
       </Link>
     </>
   );
+};
+Book.propTypes = {
+  BookData: PropTypes.any,
 };
 export default Book;
